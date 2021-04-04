9ec0c39233b45d600e9c4f666deda87dc6263130
rems64 <52427159+rems64@users.noreply.github.com>
2021-04-04 10:29:34 +0200

    Set up precompiled header

---
+++ SpringEngine/src/pch.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <vector>
+#include <map>
+#include <string>
+#include <chrono>
+#include <ofbx.h>
+#include <iostream>
+#include <memory>
+#include <fstream>
+#include <sstream>
+#include <initializer_list>
+#include <filesystem>
+
+
+#include <SpringEngine/Misc/Logger.hpp>
+
+
+#include <glm/glm.hpp>
+#include <glm/common.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
